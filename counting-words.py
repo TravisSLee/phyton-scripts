6f03890f6d389f6e2b937e5bb3adc18491fdc559
Travis Scott Lee <70967848+TravisSLee@users.noreply.github.com>
2022-06-09 16:06:46 -0400

    Update counting-words.py

--- counting-words.py
+++ counting-words.py
@@ -1,1 +1,18 @@
+print("Welcome to Counting Words!")
 
+usr_input = input("Enter a word: ")
+
+letters = dict()
+
+def counting_words(arg):
+    arg = arg.lower()
+    for i in arg:
+        if i in letters:
+            letters[i] += 1
+        else:
+            letters[i] = 1
+    return letters 
+
+counting_words(usr_input)
+print("We have analysed your word!")
+print(letters)                
