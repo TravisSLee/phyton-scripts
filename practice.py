da2a29d3b0577dd8b43582a7d66dedda7338b774
Travis Scott Lee <70967848+TravisSLee@users.noreply.github.com>
2022-06-09 11:27:10 -0400

    Update practice.py
    add practice code

--- practice.py
+++ practice.py
@@ -1,6 +1,22 @@
-data = input("Enter a single letter: ")
+# Single letter script
 
-if data.islower() == True:
-    print('It is a lower case letter.')
+# data = input("Enter a single letter: ")
+
+# if data.islower() == True:
+#     print('It is a lower case letter.')
+# else:
+#     print('It is an uppercase letter.')
+
+#Color script
+color = input("Enter a color:" )
+
+color = color.lower()
+
+if color =="red":
+    print("The color is red.")
+elif color.lower() =="green":
+    print("The color is green.")
+elif color=="yellow":
+    print("The color is yellow.")
 else:
-    print('It is an uppercase letter.')
+    print("Please enter a valid color.")
