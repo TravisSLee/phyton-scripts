4ad915d9d679da4760da7fed722901388a0ed3a2
Travis Scott Lee <70967848+TravisSLee@users.noreply.github.com>
2022-06-09 11:40:05 -0400

    Update practice.py
    More practice

--- practice.py
+++ practice.py
@@ -35,3 +35,21 @@ m_states = [s for s in states if "M" in s]
 n_states = [s for s in states if "N" in s]
 
 v_states = [s for s in states if "V" in s]
+
+# Random number checker
+
+from numpy import random
+
+random_number = random.randint(100)
+
+usr_input = input("Pick a random number from 0 - 100: ")
+
+number = int(usr_input)
+if number == random_number:
+    print("Wow, nice guess! That was it!")
+elif number > random_number:
+    print("Too bad, too high. I chose " + str(random_number))
+elif number < random_number:
+    print("Ha, nice try - too low! I chose  " + str(random_number))
+else:
+    print("Sorry try again.")
