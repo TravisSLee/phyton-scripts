309f9c5c3a6fa7f93543151681fd5fba67d54630
Travis Scott Lee <70967848+TravisSLee@users.noreply.github.com>
2022-06-09 11:28:59 -0400

    Update practice.py
    update practice

--- practice.py
+++ practice.py
@@ -1,11 +1,11 @@
 # Single letter script
 
-# data = input("Enter a single letter: ")
+data = input("Enter a single letter: ")
 
-# if data.islower() == True:
-#     print('It is a lower case letter.')
-# else:
-#     print('It is an uppercase letter.')
+if data.islower() == True:
+    print('It is a lower case letter.')
+else:
+    print('It is an uppercase letter.')
 
 #Color script
 color = input("Enter a color:" )
@@ -20,3 +20,18 @@ elif color=="yellow":
     print("The color is yellow.")
 else:
     print("Please enter a valid color.")
+
+# State finder script
+
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+
+m_states = [s for s in states if "M" in s]
+
+n_states = [s for s in states if "N" in s]
+
+v_states = [s for s in states if "V" in s]
