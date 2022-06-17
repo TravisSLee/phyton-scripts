3f756abe62cb062ebe9256cabca25ae3e01406a4
Travis Scott Lee <70967848+TravisSLee@users.noreply.github.com>
2022-06-17 12:02:35 -0400

    Update reading.py

--- reading.py
+++ reading.py
@@ -1,3 +1,5 @@
+import os
+
 def head(filepath,read_lines):
     f = open(filepath, "r")
     num_lines = sum(1 for line in open(filepath))
@@ -9,3 +11,42 @@ def head(filepath,read_lines):
     f.close()    
     
 head("flatland.txt",5000)
+
+
+def new_file(input_1, input_2, input_3):
+    
+    if os.path.exists(input_1):
+        print("The file exists.")
+        os.remove(input_1)
+        print("The file was deleted.")
+        f = open(input_1, "w")
+        print("The file was created")
+    else:
+        f = open(input_1, "w")
+        print("The file was created")
+    data = data2 = ""
+  
+    # Reading data from file1
+    with open(input_2) as fp:
+        data = fp.read()
+
+    # Reading data from file2
+    with open(input_3) as fp:
+        data2 = fp.read()
+
+    # Merging 2 files
+    # To add the data of file2
+    # from next line
+    data += "\n"
+    data += data2    
+    f.write(data)
+    f.close()
+    
+    f = open(input_1, "r")
+    print(f.read())
+
+  
+new_file( "reading.txt", "flatland.txt", "flatland01.txt")
+
+f = open(read, "r")
+print(f.read())  
