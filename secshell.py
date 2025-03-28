30cd6ab0c57a5bf9387064d5ac957bd3d0266fdc
Travis Scott Lee <70967848+TravisSLee@users.noreply.github.com>
2025-03-28 17:31:43 -0400

    Delete secshell.py

--- secshell.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-def run_src(**kwargs)
-  client = paramiko.Transport((url, port))
-  if client:
-    privKeyPath = join(dirname(abspath(file), 'example.key')
-    privKey = paramiko.Ed25519Key.from_private_key_file(filename = privKeyPath)  
-    client.connect(username, pkey)
-    session = client.open_channel(kind='session')
